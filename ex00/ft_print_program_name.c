a312f05fe200ac3715ab7c68d597f34bfca7415b
BalkamFR <pacomepilaz@gmail.com>
2025-09-29 20:53:31 +0200

    add all

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: papilaz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 11:12:37 by papilaz           #+#    #+#             */
+/*   Updated: 2025/09/25 16:11:02 by papilaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	ft_putstr(argv[0]);
+	ft_putstr("\n");
+	(void)argc;
+	return (0);
+}
