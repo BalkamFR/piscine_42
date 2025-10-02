f8d2a731e18932bf255dc55df0d0fec973b3aabf
BalkamFR <pacomepilaz@gmail.com>
2025-10-02 17:01:11 +0200

    add

--- ex05/ft_split.c
+++ ex05/ft_split.c
@@ -6,36 +6,18 @@
 /*   By: papilaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 23:13:06 by papilaz           #+#    #+#             */
-/*   Updated: 2025/10/02 16:01:18 by papilaz          ###   ########.fr       */
+/*   Updated: 2025/10/02 16:59:59 by papilaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	len_str(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
 int	compt_word(char *str, char charset)
 {
 	int	i;
 	int	len;
 
-	i = 0;
-	len = len_str(str);
-	while (str[i] != charset)
-	{
-		if (i == len)
-			return (0);
-		i++;
-	}
 	i = 0;
 	len = 0;
 	while (str[i])
@@ -67,7 +49,18 @@ int	compt_all_word(char *str, char *charset)
 	return (compt);
 }
 
-int	len_word(char *str, char charset, int index)
+int	charsetverif(char c, char *charset)
+{
+	while (*charset)
+	{
+		if (c == *charset)
+			return (1);
+		charset++;
+	}
+	return (0);
+}
+
+int	len_word(char *str, char *charset, int index)
 {
 	int	i;
 	int	len;
@@ -79,9 +72,9 @@ int	len_word(char *str, char charset, int index)
 	while (str[i])
 	{
 		compt = 0;
-		while (str[i] == charset)
+		while (charsetverif(str[i], charset) == 1)
 			i++;
-		while (str[i] != charset)
+		while (charsetverif(str[i], charset) == 0)
 		{
 			compt++;
 			i++;
@@ -97,56 +90,58 @@ char	**ft_split(char *str, char *charset)
 {
 	char	**tab_all;
 	int		i;
-	int		len_all;
 	int		a;
 
 	i = 0;
 	tab_all = malloc((sizeof(char *) * compt_all_word(str, charset)) + 1);
-	while (i < len_all)
+	while (i < compt_all_word(str, charset))
 	{
-		tab_all[i] = malloc((sizeof(char) * len_word(str, charset[0], i)) + 1);
+		tab_all[i] = malloc((sizeof(char) * len_word(str, charset, i)) + 1);
 		i++;
 	}
 	i = 0;
-	len_all = 0;
 	while (tab_all[i])
 	{
 		a = 0;
-		while (*str != charset[0])
+		while (charsetverif(*str, charset) == 0)
 		{
-			tab_all[i][a] = str[len_all];
-			len_all++;
+			tab_all[i][a] = *str;
+			str++;
 			a++;
 		}
-		tab_all[i][len_all] = '\0';
-		while (str[len_all]== charset[0])
-			len_all++;
+		while (charsetverif(*str, charset) == 1)
+			str++;
 		i++;
 	}
 	tab_all[i] = NULL;
 	return (tab_all);
 }
 
-int	main(int argc, char **argv)
+// int	main(int argc, char **argv)
+// {
+// 	int i;
+// 	char **tab;
+// 	char *str;
+// 	char *charset;
+
+// 	i = 0;
+// 	charset = "- ";
+// 	str = "c-omment ca va ?";
+// 	tab = ft_split(str, charset);
+
+// 	while (tab[i])
+// 	{
+// 		printf("%s\n", tab[i]);
+// 		free(tab[i]);
+// 		i++;
+// 	}
+// 	free(tab);
+// 	(void)argc;
+// 	(void)argv;
+// 	return (0);
+// }
+
+int	main(void)
 {
-	int i;
-	char **tab;
-	char *str;
-	char *charset;
-
-	i = 0;
-	charset = "-";
-	str = "hello-caca";
-	tab = ft_split(str, charset);
-
-	while (tab[i])
-	{
-		printf("%s\n", tab[i]);
-		free(tab[i]);
-		i++;
-	}
-	free(tab);
-	(void)argc;
-	(void)argv;
-	return (0);
+	printf("%d", compt_all_word("comm-en-t tu va ?", " -"));
 }
