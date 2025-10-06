c7974e4200c029578fb311f835c898de341c66b1
BalkamFR <pacomepilaz@gmail.com>
2025-10-06 22:56:07 +0200

    jsp pas trop j essaye de faire des test rien de tres concluant

---
+++ srcs/test.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: papilaz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 21:45:53 by papilaz           #+#    #+#             */
+/*   Updated: 2025/10/06 22:55:28 by papilaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	edit_map(int x, char **maps, char find, int value[])
+{
+	int	i;
+	int	a;
+
+	printf("%d : %d \nx = %d \n", value[0], value[1], x);
+	a = 0;
+	i = 0;
+	while (maps[i])
+	{
+		a = 0;
+		while (maps[i][a] && maps[i])
+		{
+			if (x == a)
+				maps[i][a] = '0';
+			a++;
+		}
+		i++;
+	}
+}
+
+void	find_line(char **maps, int x, char find)
+{
+	int	y;
+	int	a;
+	int	count;
+	int	value[30];
+
+	count = 0;
+	a = 0;
+	y = x;
+	while (x <= 30)
+	{
+		if (find == maps[x][0])
+		{
+			value[count] = x;
+			count++;
+		}
+		x++;
+	}
+	value[count] = 0;
+	edit_map(y, maps, find, value);
+}
