cf6c5d83b38ac398b50552cb445545a5c0eb8c46
BalkamFR <pacomepilaz@gmail.com>
2025-10-08 18:59:28 +0200

    jesaispaw a vie f

--- srcs/main.c
+++ srcs/main.c
@@ -3,29 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: equentin <equentin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: papilaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 11:27:03 by equentin          #+#    #+#             */
-/*   Updated: 2025/10/08 18:34:56 by equentin         ###   ########.fr       */
+/*   Updated: 2025/10/08 18:45:37 by papilaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/params.h"
-#include "../includes/map.h"
-#include "../includes/utils.h"
 #include "../includes/free.h"
+#include "../includes/map.h"
+#include "../includes/params.h"
+#include "../includes/utils.h"
 #include <stdlib.h>
 
-int	main(int argc, char **argv)
+void	read_all_map(char *name_map)
 {
 	char		**map;
 	int			**square;
 	t_params	*params;
 	t_coords	coords;
 
-	(void)argc;
-	(void)argv;
-	map = read_file("test");
+
+	map = read_file(name_map);
 	params = malloc(sizeof(t_params));
 	square = NULL;
 	if (write_params(params, map) && is_map_valid(map, params))
@@ -39,5 +38,24 @@ int	main(int argc, char **argv)
 	else
 		ft_print("map error\n", 2);
 	free_all(map, params, square);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc == 1)
+	{
+		/* code */
+	}
+	if (argc > 1)
+	{
+		while (argv[i])
+		{
+			read_all_map(argv[i]);
+			i++;
+		}
+	}
 	return (0);
 }
--- srcs/params.c
+++ srcs/params.c
@@ -3,23 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   params.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: equentin <equentin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: papilaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 08:33:08 by equentin          #+#    #+#             */
-/*   Updated: 2025/10/08 18:25:04 by equentin         ###   ########.fr       */
+/*   Updated: 2025/10/08 18:57:33 by papilaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "../includes/params.h"
 #include "../includes/utils.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int	is_param_ok(char *str)
 {
-	int		i;
-	int		j;
+	int	i;
+	int	j;
 
 	i = 0;
 	while (str[i])
@@ -45,11 +46,16 @@ int	is_param_ok(char *str)
 
 int	write_params(t_params *params, char **map)
 {
-	int	params_len;
+	int		params_len;
+	char	jsp[4];
 
-	if (is_param_ok(map[0]) == 0)
-		return (0);
 	params_len = ft_strlen(map[0]);
+	jsp[0] = map[0][params_len - 1];
+	jsp[1] = map[0][params_len - 2];
+	jsp[2] = map[0][params_len - 3];
+	jsp[3] = '\0';
+	if (is_param_ok(jsp) == 0)
+		return (0);
 	params->max_line = ft_strlen(map[1]);
 	params->full = map[0][params_len - 1];
 	params->block = map[0][params_len - 2];
